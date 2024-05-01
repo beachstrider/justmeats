91a37d471f6c5ecbff6887932ce8e3ef62448245
Beach Strider <mernstackdev729@gmail.com>
2024-05-01 14:23:18 +0000

    remove: console log

--- app/lib/configLuckyOrange.js
+++ app/lib/configLuckyOrange.js
@@ -3,13 +3,11 @@ export const configLuckyOrange = (customer) => {
   const customerName = `${customer?.firstName} ${customer?.lastName}`
 
   if (customerEmail && customerName) {
-    console.log('LO started', customerEmail, customerName)
     window.LO.$internal.ready('visitor').then(() => {
       window.LO.visitor.identify({
         Email: customerEmail,
         Name: customerName,
       })
-      console.log('LO ended')
     })
   }
 }
