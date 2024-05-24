c89e0e3fb7265f494d4eec5a0c21343e30caae8e
Max Holloway <max@justmeats.com>
2024-05-23 23:00:20 -0600

    update: filter dropdown focus color

--- app/components/Dropdown.jsx
+++ app/components/Dropdown.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
-import { Down } from '~/icons/Down'
 import { cn } from '~/lib/utils'
 
 export const Dropdown = ({
@@ -11,13 +10,32 @@ export const Dropdown = ({
   menuMinWidth = 100,
   align = 'start',
 }) => {
+  const [isOpened, setIsOpened] = useState(false)
+
+  const DownIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="none">
+      <path
+        d="M9.3335 13.3333L16.0002 20L22.6668 13.3333"
+        stroke={isOpened ? 'white' : '#7A392D'}
+        strokeWidth={3}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  )
+
   return (
-    <DropdownMenu.Root>
+    <DropdownMenu.Root onOpenChange={(v) => setIsOpened(v)}>
       <DropdownMenu.Trigger asChild>
-        <button className="flex justify-between items-center font-dunbar font-medium sm:text-[14px] py-[4px] pl-[14px] pr-[10px] rounded-[4px] border-2 border-[#7A392D] text-[#7A392D] tracking-[0.7px]">
+        <button
+          className={cn(
+            'flex justify-between items-center font-dunbar font-medium sm:text-[14px] py-[4px] pl-[14px] pr-[10px] rounded-[4px] border-2 border-[#7A392D] tracking-[0.7px]',
+            isOpened ? 'bg-[#7A392D] text-white' : 'text-[#7A392D]',
+          )}
+        >
           <div>{placeholder}</div>
-          <div className="w-[22px]">
-            <Down />
+          <div className={'w-[22px]'}>
+            <DownIcon />
           </div>
         </button>
       </DropdownMenu.Trigger>
