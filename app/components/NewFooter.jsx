ee4f5b8d898d67907db54d001ab7ed8374e755f1
Max Holloway <max@justmeats.com>
2024-06-10 13:46:45 -0600

    update: new footer

--- app/components/NewFooter.jsx
+++ app/components/NewFooter.jsx
@@ -2,25 +2,32 @@ import { useLocation } from 'react-router-dom'
 
 import { NavLink, useMatches } from '@remix-run/react'
 
+import { Facebook } from '~/icons/Facebook'
+import { Instagram } from '~/icons/Instagram'
 import { LogoWhite } from '~/icons/LogoWhite'
+import { Mail } from '~/icons/Mail'
+import { Phone } from '~/icons/Phone'
+import { Tiktok } from '~/icons/Tiktok'
 
 export const Footer = () => {
   const matches = useMatches()
   const location = useLocation()
-  const isSpecialsPage = location.pathname === '/gym-launch' || location.pathname === '/gym'
+  const isSpecialsPage =
+    location.pathname === '/gym-launch' || location.pathname === '/gym'
+  const page = matches.at(-1).pathname
 
   return (
     <footer className={`${isSpecialsPage ? 'bg-[#6B1626]' : 'bg-[#231b19]'}`}>
       <div className="container-small relative h-[88px] sm:h-[128px] flex items-center justify-between py-4">
         <div className="absolute-center">
           <NavLink to="/" end prefetch="intent">
-            <div className="w-[148px] sm:w-[214px]">
+            <div className="w-[160px] sm:w-[196px]">
               <LogoWhite />
             </div>
           </NavLink>
         </div>
       </div>
-      {matches.at(-1).pathname === '/rich-froning' && (
+      {page === '/rich-froning' && (
         <div className="bg-lower-footer bg-cover text-center sm:[background-position-x:0] [background-position-x:-750px]">
           <div className="container-small relative text-[#EFEEED] sm:pt-[80px] sm:pb-[92px] pt-[113px] pb-[137px] lg:pl-0 lg:pr-0">
             <div className="sm:text-[18px] sm:leading-[26px] sm:tracking-[0.36px] sm:text-center text-justify [word-spacing:-1px] sm:[word-spacing:0] text-[16px] leading-[150%] tracking-[0.48px] font-nunito sm:mb-[16px] mb-[18px]">
@@ -38,6 +45,104 @@ export const Footer = () => {
           </div>
         </div>
       )}
+      <div className="bg-[#6B1626] text-[#EFEEED] sm:pt-[55px] sm:pb-[25px] pt-[48px] pb-[22px]">
+        <div className="container-small">
+          <div className="max-w-[740px] w-full mx-auto flex justify-center">
+            <div className="flex flex-col sm:gap-[53px] gap-[30px]">
+              <div className="flex sm:flex-row flex-col sm:gap-[128px] gap-[35px]">
+                <div>
+                  <div className="font-bold tracking-[0.8px] sm:mb-[22px] mb-[18px]">
+                    ABOUT US
+                  </div>
+                  <div className="flex flex-col sm:text-[14px] text-[16px] font-bold sm:leading-[170%] leading-[190%]">
+                    <NavLink end prefetch="intent" to="/products/custom-bundle">
+                      Menu
+                    </NavLink>
+                    <NavLink end prefetch="intent" to="/about">
+                      How It Works
+                    </NavLink>
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold tracking-[0.8px] sm:mb-[22px] mb-[18px]">
+                    NEED HELP?
+                  </div>
+                  <div className="flex flex-col sm:text-[14px] text-[16px] font-bold sm:leading-[170%] leading-[190%]">
+                    <NavLink to="#">FAQs</NavLink>
+                    <NavLink end prefetch="intent" to="/term-services">
+                      Terms of Service
+                    </NavLink>
+                    <NavLink end prefetch="intent" to="/refund-policy">
+                      Refund & Cancellation Policy
+                    </NavLink>
+                    <NavLink end prefetch="intent" to="/privacy-policy">
+                      Privacy Policy
+                    </NavLink>
+                  </div>
+                </div>
+                <div>
+                  <div className="font-bold tracking-[0.8px] sm:mb-[22px] mb-[18px]">
+                    CONTACT US
+                  </div>
+                  <div className="flex flex-col sm:text-[14px] text-[16px] font-bold sm:leading-[170%] leading-[190%]">
+                    <a
+                      href="tel:+18883431242"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex items-center gap-[8px]"
+                    >
+                      <div className="w-[22px]">
+                        <Phone />
+                      </div>
+                      <div>888-343-1242</div>
+                    </a>
+                    <a
+                      href="mailto:support@justmeats.com"
+                      target="_blank"
+                      rel="noreferrer"
+                      className="flex items-center gap-[8px]"
+                    >
+                      <div className="w-[22px] flex justify-center items-center">
+                        <div className="w-[18px]">
+                          <Mail />
+                        </div>
+                      </div>
+                      <div>support@justmeats.com</div>
+                    </a>
+                    <div className="flex items-center gap-[4px] mt-[16px] ml-[-6px]">
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://www.facebook.com/profile.php?id=61550191665305"
+                      >
+                        <Facebook />
+                      </a>
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://www.instagram.com/justmeats.co/"
+                      >
+                        <Instagram />
+                      </a>
+                      <a
+                        target="_blank"
+                        rel="noreferrer"
+                        href="https://www.tiktok.com/@justmeats.com"
+                        className="ml-[8px]"
+                      >
+                        <Tiktok />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="text-center">
+                ©2023 JUST MEATS, All Rights Reserved
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </footer>
   )
 }
---
+++ app/icons/Facebook.jsx
@@ -0,0 +1,14 @@
+export const Facebook = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="36"
+    height="36"
+    viewBox="0 0 36 36"
+    fill="none"
+  >
+    <path
+      d="M19.5 15H24V19.5H19.5V30H15V19.5H10.5V15H15V13.1175C15 11.334 15.561 9.081 16.677 7.8495C17.793 6.615 19.1865 6 20.856 6H24V10.5H20.85C20.103 10.5 19.5 11.103 19.5 11.8485V15Z"
+      fill="#EFEEED"
+    />
+  </svg>
+)
