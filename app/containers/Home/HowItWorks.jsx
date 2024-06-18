c7edacbd5738e8659db8dec0bd2abeaf70863d81
Beach Strider <48445639+beachstrider@users.noreply.github.com>
2024-06-18 12:54:00 -0600

    test2

--- app/containers/Home/HowItWorks.jsx
+++ app/containers/Home/HowItWorks.jsx
@@ -5,18 +5,20 @@ import { HowItWorksStepsSlider } from '~/components/HowItWorksStepsSlider'
 
 export const HowItWorks = () => {
   return (
-    <section className="relative flex flex-col items-center bg-yellow-pattern">
+    <section className="relative flex flex-col items-center overflow-x-hidden bg-yellow-pattern">
       <img
         src={cow}
         alt=""
         className="absolute top-[-46px] right-0 hidden sm:block"
       />
-      <div className="md:pt-[62px] pt-[54px] sm:pb-[60px] pb-[60px]">
+      <div className="md:pt-[62px] pt-[54px] sm:pb-[60px] pb-[60px] w-full">
         <div className="sm:text-[14px] font-bold leading-[23px] sm:tracking-[2.8px] text-[12px] tracking-[2.8px] text-[#231B19] text-center">
           HOW JUST MEATS WORKS
         </div>
-        <div className="text-center text-[#6B1626] sm:text-[36px] text-[24px] font-bold leading-tight sm:tracking-[1.8px] tracking-[1.2px] sm:mb-[20px] mb-[17px] font-hudson">
-          THE ULTI-MEAT EXPERIENCE
+        <div className="container-small">
+          <div className="text-center text-[#6B1626] sm:text-[36px] text-[24px] font-bold leading-tight sm:tracking-[1.8px] tracking-[1.2px] sm:mb-[20px] mb-[17px] font-hudson">
+            THE ULTI-MEAT EXPERIENCE
+          </div>
         </div>
         <div className="flex justify-center sm:mb-[43px] mb-[35px] sm:px-0 px-[12px]">
           <div className="max-w-[645px] text-[#231b19] sm:text-center text-justify [word-spacing:-1px] sm:[word-spacing:0] sm:text-[18px] text-[16px] font-normal sm:leading-[26px] leading-[25px] tracking-[0.16px]">
