434ddddf964f596e12c9f38f9b41043532d4d27e
Beach Strider <48445639+beachstrider@users.noreply.github.com>
2024-06-19 15:59:53 -0600

    fix: aside cart scroll content height

--- app/containers/CustomBundle/Cart/CartLines.jsx
+++ app/containers/CustomBundle/Cart/CartLines.jsx
@@ -16,7 +16,7 @@ export function CartLines({ type }) {
   }
 
   return (
-    <div className="lg:pl-[40px] lg:pt-[30px] lg:pb-[14px] pl-[20px] pt-[20px] pb-[20px] lg:pr-[24px] pr-[20px] md:mr-[16px] mr-0 overflow-x-hidden cart_lines_scrollbar lg:mb-0 mb-[374px] h-[calc(100vh-250px)]">
+    <div className="lg:pl-[40px] lg:pt-[30px] lg:pb-[14px] pl-[20px] pt-[20px] pb-[20px] lg:pr-[24px] pr-[20px] md:mr-[16px] mr-0 overflow-x-hidden cart_lines_scrollbar lg:mb-0 mb-[374px] h-[calc(100vh-270px)]">
       <div className="grid grid-cols-3 lg:gap-[22px] gap-[8px] lg:grid-cols-1">
         <CartLineItem line={freeProduct} type={type} lineType="free" />
         <CartLineItem line={bonusLine} type={type} lineType="bonus" />
