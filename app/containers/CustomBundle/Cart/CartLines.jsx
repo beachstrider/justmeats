98e6a1ca9988024fa646e292ea06b744f7de6d12
Beach Strider <48445639+beachstrider@users.noreply.github.com>
2024-06-19 15:47:16 -0600

    add: aside cart inner scroll

--- app/containers/CustomBundle/Cart/CartLines.jsx
+++ app/containers/CustomBundle/Cart/CartLines.jsx
@@ -16,15 +16,13 @@ export function CartLines({ type }) {
   }
 
   return (
-    <div className="lg:pl-[40px] lg:pt-[30px] lg:pb-[14px] pl-[20px] pt-[20px] pb-[20px] overflow-x-hidden cart_lines_scrollbar lg:mb-0 mb-[374px]">
-      <div className="lg:pr-[40px] pr-[20px]">
-        <div className="grid grid-cols-3 lg:gap-[22px] gap-[8px] lg:grid-cols-1">
-          <CartLineItem line={freeProduct} type={type} lineType="free" />
-          <CartLineItem line={bonusLine} type={type} lineType="bonus" />
-          {selectedProducts.map((product) => (
-            <CartLineItem key={product.id} type={type} line={product} />
-          ))}
-        </div>
+    <div className="lg:pl-[40px] lg:pt-[30px] lg:pb-[14px] pl-[20px] pt-[20px] pb-[20px] lg:pr-[24px] pr-[20px] md:mr-[16px] mr-0 overflow-x-hidden cart_lines_scrollbar lg:mb-0 mb-[374px] h-[calc(100vh-250px)]">
+      <div className="grid grid-cols-3 lg:gap-[22px] gap-[8px] lg:grid-cols-1">
+        <CartLineItem line={freeProduct} type={type} lineType="free" />
+        <CartLineItem line={bonusLine} type={type} lineType="bonus" />
+        {selectedProducts.map((product) => (
+          <CartLineItem key={product.id} type={type} line={product} />
+        ))}
       </div>
     </div>
   )
