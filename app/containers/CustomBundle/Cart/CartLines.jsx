bff62785a92442a45199b6c601da3e682901dc83
Beach Strider <48445639+beachstrider@users.noreply.github.com>
2024-06-21 15:19:42 -0600

    fix: missing free raspberry in shopping page

--- app/containers/CustomBundle/Cart/CartLines.jsx
+++ app/containers/CustomBundle/Cart/CartLines.jsx
@@ -19,10 +19,9 @@ export function CartLines({ type }) {
   return (
     <div className="lg:pl-[40px] lg:pt-[30px] lg:pb-[14px] pl-[20px] pt-[20px] pb-[20px] lg:pr-[24px] pr-[20px] md:mr-[16px] mr-0 overflow-x-hidden cart_lines_scrollbar lg:mb-0 mb-[374px] lg:h-[calc(100vh-270px)] h-[calc(100vh-245px)]">
       <div className="grid grid-cols-3 lg:gap-[22px] gap-[8px] lg:grid-cols-1">
-        {isCartPage ||
-          (!isCartPage && isFreeProductSubscribed && (
-            <CartLineItem line={freeProduct} type={type} lineType="free" />
-          ))}
+        {(isCartPage || (!isCartPage && isFreeProductSubscribed)) && (
+          <CartLineItem line={freeProduct} type={type} lineType="free" />
+        )}
         <CartLineItem line={bonusLine} type={type} lineType="bonus" />
         {selectedProducts.map((product) => (
           <CartLineItem key={product.id} type={type} line={product} />
