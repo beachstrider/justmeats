953b777d3aad06961129eaec8b42aad0c89713ca
Beach Strider <48445639+beachstrider@users.noreply.github.com>
2024-06-27 14:09:38 -0600

    update recipes data

--- app/data/recipes.js
+++ app/data/recipes.js
@@ -532,4 +532,62 @@ export const recipes = [
     updated: 'May 13, 2024',
     type: 'bison',
   },
+  {
+    url: 'recipe11',
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="28"
+        height="28"
+        viewBox="0 0 28 28"
+        fill="none"
+      >
+        <g clipPath="url(#clip0_5095_3714)">
+          <path
+            d="M14 2.33331C20.4435 2.33331 25.6667 7.55648 25.6667 14C25.6667 20.4435 20.4435 25.6666 14 25.6666C7.55651 25.6666 2.33334 20.4435 2.33334 14C2.33334 7.55648 7.55651 2.33331 14 2.33331ZM14 4.66665C11.5247 4.66665 9.15069 5.64998 7.40035 7.40032C5.65001 9.15066 4.66668 11.5246 4.66668 14C4.66668 16.4753 5.65001 18.8493 7.40035 20.5996C9.15069 22.35 11.5247 23.3333 14 23.3333C16.4754 23.3333 18.8493 22.35 20.5997 20.5996C22.35 18.8493 23.3333 16.4753 23.3333 14C23.3333 11.5246 22.35 9.15066 20.5997 7.40032C18.8493 5.64998 16.4754 4.66665 14 4.66665ZM14 6.99998C14.2858 7.00002 14.5616 7.10493 14.7751 7.29481C14.9887 7.4847 15.1251 7.74635 15.1585 8.03015L15.1667 8.16665V13.517L18.3248 16.6751C18.5341 16.8851 18.6556 17.1668 18.6646 17.4631C18.6737 17.7594 18.5696 18.048 18.3736 18.2703C18.1775 18.4926 17.9042 18.632 17.6091 18.6601C17.314 18.6882 17.0193 18.603 16.7848 18.4216L16.6752 18.3248L13.1752 14.8248C12.9939 14.6433 12.8774 14.4071 12.8438 14.1528L12.8333 14V8.16665C12.8333 7.85723 12.9563 7.56048 13.1751 7.34169C13.3938 7.1229 13.6906 6.99998 14 6.99998Z"
+            fill="#BF4745"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_5095_3714">
+            <rect width="28" height="28" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ),
+    imgs: [
+      'https://res.cloudinary.com/meals/image/upload/v1719513859/Web_Regular-JustMeats_ChimichuriSteakBreakfastCroissant_Stills_FullRes_202024APR09_SethH-3_1.jpg',
+      'https://res.cloudinary.com/meals/image/upload/v1719513852/Web_Regular-JustMeats_ChimichuriSteakBreakfastCroissant_Stills_FullRes_202024APR09_SethH-2_2.jpg',
+    ],
+    header: 'Chimmichuri Steak Breakfast Croissant',
+    text: 'Enjoy Just Meats Chimichurri Steak, bacon, egg, avocado, in a buttery croissant. A Perfect indulgence!',
+    recipepreptime_text: 'Prep time',
+    recipepreptime_value: '15 min',
+    recipetotaltime_text: 'Total time',
+    recipetotaltime_value: '15 min',
+    recipeserving_text: 'Servings',
+    recipeserving_value: '1',
+    paragraph:
+      'In just 15 minutes, I crafted the ultimate breakfast sandwich with Just Meats Chimichurri Steak. I started by cooking the egg and bacon to perfection, then heated the flavorful steak. I buttered a warm croissant, added creamy avocado slices, and layered on the steak, egg, and bacon. With each bite, the rich flavors of the steak and the crispiness of the bacon combined beautifully with the buttery croissant and smooth avocado. An indulgent treat I truly savored!',
+    ingredients1: '1',
+    ingredients1_value: 'large croissant',
+    ingredients2: '1/4',
+    ingredients2_value:
+      "tray of<span class='text-[#6B1626] font-bold'>Just Meats Chimichurri Steak and marinade</span>",
+    ingredients3: '2',
+    ingredients3_value: '2 strips of bacon',
+    ingredients4: '1',
+    ingredients4_value: 'egg',
+    ingredients5: '1tsp',
+    ingredients5_value: 'Butter',
+    ingredients6: '1/4',
+    ingredients6_value: 'an avocado',
+    direction_step1:
+      "<div> Cook the egg, and bacon as desired. Heat the <span class='text-[#6B1626] font-bold'>Chimichurri Steak</span> with marinade for 2-3 minutes.</div>",
+    direction_step2:
+      'Cut the croissant in half and butter both sides. Add the sliced avocado on one side, then top with the Chimichurri Steak, egg and bacon.',
+    direction_step3: 'Add the top of the croissant, cut in half and enjoy!',
+    updated: 'June 13, 2024',
+    type: 'beef',
+  },
 ]
