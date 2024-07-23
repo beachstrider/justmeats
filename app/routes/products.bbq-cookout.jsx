6e63637a400b9d917c81009156ef691c6e2eb312
Beach Strider <48445639+beachstrider@users.noreply.github.com>
2024-07-23 11:36:42 -0600

    add blank page

---
+++ app/routes/products.bbq-cookout.jsx
@@ -0,0 +1,16 @@
+export default function BbqCookout() {
+  return (
+    <main className="bg-[#F8F2E8] lg:pt-[72px] lg:pb-[90px] pt-[36px] pb-[50px]">
+      <div className="container-small">
+        <div className="flex lg:flex-row flex-col lg:gap-[20px]">
+          <div className="hidden lg:block bg-img-2"></div>
+          <div className="">
+            <div className="lg:pt-[27px] pt-[27px] lg:pb-[37px] pb-[30px]">
+              ..........
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
