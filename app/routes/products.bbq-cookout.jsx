015caba0e320c7ef26318eddb3b37715b6bc4441
Beach Strider <48445639+beachstrider@users.noreply.github.com>
2024-07-23 14:21:27 -0600

    add basic widget

--- app/routes/products.bbq-cookout.jsx
+++ app/routes/products.bbq-cookout.jsx
@@ -3,10 +3,32 @@ export default function BbqCookout() {
     <main className="bg-[#F8F2E8] lg:pt-[72px] lg:pb-[90px] pt-[36px] pb-[50px]">
       <div className="container-small">
         <div className="flex lg:flex-row flex-col lg:gap-[20px]">
-          <div className="hidden lg:block bg-img-2"></div>
-          <div className="">
-            <div className="lg:pt-[27px] pt-[27px] lg:pb-[37px] pb-[30px]">
-              ..........
+          <div className="hidden w-full lg:block bg-img-2"></div>
+          <div className="lg:max-w-[456px] w-full">
+            <div className="lg:pt-[27px] pt-[27px] lg:pb-[37px] pb-[30px] bg-white lg:px-[36px] px-[32px] text-center">
+              <div className="font-hudson font-bold leading-none lg:text-[32px] text-[24px] lg:tracking-[1.6px] tracking-[1.2px]">
+                BBQ ENTRY ONLY
+              </div>
+              <div className="font-bold leading-[150%] lg:text-[18px] text-[16px] lg:mb-[16px] mb-[8px]">
+                Texas BBQ - Fun - Good Company
+              </div>
+              <div className="text-[16px] leading-[1.3] lg:mb-[22px] mb-[18px]">
+                We’re hosting a BBQ cookout and you’re invited! Just Meats & Pit
+                Boss have partnered up to bring you the Ulti-Meat Texas BBQ
+                experience.
+              </div>
+              <div className="font-bold leading-[20px] lg:text-[18px] text-[16px] text-[#BF4745] lg:mb-[22px] mb-[18px] max-w-[280px] mx-auto">
+                Join Just Meats, Pit Boss, The Buttery Bros, and More!
+              </div>
+              <div className="text-[16px] leading-[1.3]">
+                Come hang with the Just Meats and Pit Boss crews while enjoying
+                a meal chock-full of delicious food prepared by our
+                award-winning pitmasters. You can also delight in the fun and
+                atmosphere the Buttery Bros and Podium Nutrition will provide.
+              </div>
+            </div>
+            <div className="pt-[24px] lg:pb-[26px] pb-[40px] bg-[#6b1626] text-white lg:px-[30px] px-[42px]">
+              ...
             </div>
           </div>
         </div>
