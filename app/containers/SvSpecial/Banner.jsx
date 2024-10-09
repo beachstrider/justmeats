b6bd68ac4619fd31f74ed6e79bffe083de8c11ed
tank <tank@justmeats.com>
2024-10-10 00:45:05 +0530

    Added announcement bar

--- app/containers/SvSpecial/Banner.jsx
+++ app/containers/SvSpecial/Banner.jsx
@@ -8,6 +8,22 @@ export const Banner = () => {
           <img src={StayFresh} alt="" className="m-auto" />
         </div>
       </div>
+      <div class="bg-[#ad916b] text-white p-2 text-center">
+        <p class="sm:block hidden font-semibold text-[18px] font-[baskerville-poster-pt] uppercase tracking-widest italic">
+          U.S.D.A. Choice Steaks, Cut To Order—Shipping October 21-25
+        </p>
+        <div class="sm:hidden block text-center">
+          <p class="text-[18px] font-semibold font-[baskerville-poster-pt] uppercase tracking-widest italic">
+            U.S.D.A. Choice Steaks,
+          </p>
+          <p class="text-[18px] font-semibold font-[baskerville-poster-pt] uppercase tracking-widest italic text-[#000000]">
+            Cut To Order—
+          </p>
+          <p class="text-[18px] font-semibold font-[baskerville-poster-pt] uppercase tracking-widest italic">
+            Shipping October 21-25
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
